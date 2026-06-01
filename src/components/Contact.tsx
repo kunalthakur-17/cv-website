07a7cc6aa7d8b35ed724fc7f64de8804ad113516
kunalthakur-17 <kunalkharyal@gmail.com>
2026-06-01 17:40:53 +0530

    Complete portfolio

---
+++ src/components/Contact.tsx
@@ -0,0 +1,61 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="mailto:kunalthakur306@gmail.com" data-cursor="disable">
+                kunalthakur306@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>BCA - Lovely Professional University</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/kunalthakur-17"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kunalthakur306/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/KunalTh80622705"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Kunal Thakur</span>
+            </h2>
+            <h5>
+              <MdCopyright /> {new Date().getFullYear()}
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
